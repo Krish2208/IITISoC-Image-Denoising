7293b3c61d31931accaa666aa0d77128865ddeb8
Rupalshah17 <shah17rupal@gmail.com>
2022-08-02 18:04:18 +0530

    Basic website structure

---
+++ frontend/src/components/home.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./home.css";
+import background from "../black_bg.png";
+import logo from "../logo.png";
+
+export default function home() {
+  return (
+    <>
+      <nav class="navbar">
+        <div className="navtext">Image Denoiser: ADNet Model</div>
+      </nav>
+      <div
+        style={{
+          backgroundImage: `url(${background})`,
+          width: "100vw",
+          height: "100vh",
+          backgroundSize: "cover",
+        }}
+      >
+        <img src={logo} className="logo" alt="Logo" />
+        <a href="/app" className="link">
+          <div className="startlink">
+            Get Started
+            <FontAwesomeIcon
+              icon="fa-solid fa-circle-chevron-right"
+              style={{ paddingLeft: 5 }}
+            />
+          </div>
+        </a>
+      </div>
+    </>
+  );
+}
---
+++ frontend/src/components/model.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./model.css";
+
+export default function model() {
+  return (
+    <>
+      <div class="split left">
+        <div className="left_heading">
+          <h1>Upload/Image Capture</h1>
+        </div>
+      </div>
+
+      <div class="split right">
+        <div className="right_heading">
+                  <h1>ADNet Image Denoising Model
+                </h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
